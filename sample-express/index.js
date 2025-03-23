8a36e0b0f16fff06dece1ae555dba3347fe13d5a
mjohop88 <49928982+mjohop88@users.noreply.github.com>
2025-03-23 18:00:47 -0400

    typescript conversion

--- sample-express/index.js
+++ sample-express/index.js
@@ -1,30 +1,39 @@
 // Loads express, instantiates app, and defines a constant for port use
-
-import { routeHello, routeAPINames } from "./routes.js";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { routeHello, routeAPINames, routeWeather } from "./routes.js";
 import express from "express";
-
 const server = express();
 const port = 3000;
-
 //Creates routine to respond to GET requests
-
-server.get('/hello', function (req,res) {
-    const response = routeHello(req, res);
-    res.send('Hello World!');
-});
-
-server.get("/api/names", async function (req, res) {
-    let response;
-    try {
-        response = await routeAPINames(req, res);
-    } catch (err) {
-        console.log(err);
-    }
+server.get("/hello", function (_req, res) {
+    const response = routeHello();
     res.send(response);
 });
-
+server.get("/api/names", function (_req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response;
+        try {
+            response = yield routeAPINames();
+            res.send(response);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+});
 //Spins up server and tells it to listen on specified port defined above
-
-server.listen(port, function() {
+server.get("/api/weather/:zipcode", function (req, res) {
+    const response = routeWeather({ zipcode: req.params.zipcode });
+    res.send(response);
+});
+server.listen(port, function () {
     console.log('Listening on ' + port);
 });
---
+++ sample-express/index.ts
@@ -0,0 +1,40 @@
+// Loads express, instantiates app, and defines a constant for port use
+
+import { routeHello, routeAPINames, routeWeather } from "./routes.js";
+import express, { Request, Response } from "express";
+
+const server = express();
+const port = 3000;
+
+//Creates routine to respond to GET requests
+
+server.get("/hello", function (_req: Request, res: Response): void {
+    const response = routeHello();
+    res.send(response);
+});
+
+server.get("/api/names", 
+    async function (_req: Request, res: Response): Promise<void> {
+        let response: string;
+        try {
+            response = await routeAPINames();
+            res.send(response);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
+
+//Spins up server and tells it to listen on specified port defined above
+
+server.get(
+    "/api/weather/:zipcode",
+    function (req: Request, res: Response): void {
+        const response = routeWeather({ zipcode: req.params.zipcode });
+        res.send(response);
+    }
+)
+
+server.listen(port, function (): void {
+    console.log('Listening on ' + port);
+});
